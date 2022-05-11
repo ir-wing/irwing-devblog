4b42cef50bcb3b296ea442fb0ec94ab2d5575783
irwing <60259504+ir-wing@users.noreply.github.com>
2022-05-11 10:40:59 +0300

    Update blogloader.js

--- blogloader.js
+++ blogloader.js
@@ -16,18 +16,21 @@ window.onload = function() {
           var title = findChild(e, "blogTitle");
           var image = findChild(e, "blogImage");
           if (bd) {
+                    title.textContent = bd.result.title;
                     for (var _index = 0 ; _index < bd.result.content.length; _index++) {
                               if (bd.result.content.tag == "p") {
                                         var _text = text.cloneNode(true);
                                         text.parentNode.AppendChild(_text);
                                         _text.textContent = bd.result.content.children[0];
                               }
-                              if (bd.result.content.tag == "img") {
-                                        var _text = text.cloneNode(true);
-                                        text.parentNode.AppendChild(_text);
-                                        _text.textContent = bd.result.content.children[0];
+                              if (bd.result.content.tag == "figure") {
+                                        var _image = image.cloneNode(true);
+                                        image.parentNode.AppendChild(_image);
+                                        _image.src = bd.result.content.children[0].attrs.src;
                               }
                     }
           }
+          text.remove();
+          image.remove();
           
 }
