5bbef2e81a76afda996e5e4acc3ccf5c6dfa35f2
irwing <60259504+ir-wing@users.noreply.github.com>
2022-05-11 20:03:53 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,6 +1,10 @@
 var blogs = [
-          "https://api.telegra.ph/getPage/test-05-11-193"
+          "https://api.telegra.ph/getPage/test-05-11-193?return_content=true",
+
+
+         
 ]
+
 
 var findChild = function(node,name)
 {
@@ -12,37 +16,47 @@ var findChild = function(node,name)
           }
 }
 
+var MainPage = function()
+{
+          window.open('index.html',"_self");
+}
+
 
 window.onload = function() { // When the webpage loads
-         
+          //deleting prev blog history
+          localStorage.clear();
+
 
           //loading example blogs
           var div = document.getElementById("exampleBlog");
+          var divParent = div.parentNode;
+          for (var index = 0; index < blogs.length;index++) {
           
-          for (var index = 0; index < blogs.length; index++) {
-                    var content = ""; 
 
-                    var exampleDiv = div.cloneNode(true);
-                    div.parentNode.appendChild(exampleDiv);
-                    var title = findChild(exampleDiv, "blogTitle");
-                    var image = findChild(exampleDiv, "blogImage");
-                    var desc = findChild(exampleDiv, "blogDescription");
-                    var openButton = findChild(exampleDiv, "right button read");
-                    var link = blogs[index]; //Getting the link string
-                    var request = new XMLHttpRequest(); //Creating a request
-                    request.open("GET",link + "?return_content=true");
-                    request.send();
-
-                    request.onload = function() {
-                              var data = JSON.parse(request.responseText); //the data
-                              
-                              
-                              
                     
+                    var link = blogs[index]; //Getting the link string
+                    
+                    fetch(link)
+                    .then((response) => {
+                              return response.json();
+                    })
+                    .then((data) => {
+                              //var data = data; //the data
+                              console.log(data);
+
+                              var exampleDiv = div.cloneNode(true);
+                              divParent.appendChild(exampleDiv);
+                              var title = findChild(exampleDiv, "blogTitle");
+                              var image = findChild(exampleDiv, "blogImage");
+                              var desc = findChild(exampleDiv, "blogDescription");
+                              var openButton = findChild(exampleDiv, "right button read"); 
                               
+                                        
                               image.src = data.result.image_url; 
-                              title.textContent = data.result.title;
-                              desc.textContent = data.result.description;
+                              title.innerHTML = data.result.title;
+                              desc.innerHTML = data.result.description;
+                               
+                              
                               openButton.onclick = function()
                               {
                                         localStorage.setItem("blogData",JSON.stringify(data));
@@ -50,9 +64,16 @@ window.onload = function() { // When the webpage loads
                                         
                                         
                               }
-                    }
+                              
+                    })
+                    
+               
+                    
                               
                     
           }
           div.remove();
+          
+          
+          
 }
