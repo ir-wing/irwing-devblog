0a67bb8a8775510b2c1342e01cde2a54d189d20d
irwing <60259504+ir-wing@users.noreply.github.com>
2022-05-11 11:32:29 +0300

    fixed [Object object] + images

--- blogloader.js
+++ blogloader.js
@@ -18,7 +18,7 @@ window.onload = function() {
           if (bd) {
                     title.textContent = bd.result.title;
                     for (var _index = 0 ; _index < bd.result.content.length; _index++) {
-                              if (bd.result.content[_index].tag == "p") {
+                              if (bd.result.content[_index].tag == "p" && bd.result.content[_index].children[0].tag != "br") {
                                         var _text = text.cloneNode(true);
                                         text.parentNode.appendChild(_text);
                                         _text.textContent = bd.result.content[_index].children[0];
@@ -26,7 +26,7 @@ window.onload = function() {
                               if (bd.result.content[_index].tag == "figure") {
                                         var _image = image.cloneNode(true);
                                         image.parentNode.appendChild(_image);
-                                        _image.src = bd.result.content[_index].children[0].attrs.src;
+                                        _image.src = bd.result.content[_index].children[0][0].attrs.src;
                               }
                     }
           }
