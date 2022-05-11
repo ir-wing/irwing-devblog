bd5ffc42994a3b04da54fe54660654995ccc44ed
irwing <60259504+ir-wing@users.noreply.github.com>
2022-05-11 20:17:55 +0300

    Update blogloader.js

--- blogloader.js
+++ blogloader.js
@@ -13,16 +13,13 @@ var MainPage = function()
           window.open('index.html',"_self");
 }
 window.onload = function() {
-          
+
           var bd = JSON.parse(localStorage.getItem("blogData"));
-          var e = document.getElementById("exampleBlog");
-          console.log(bd);
-          var text = findChild(e, "blogContent");
-          var title = findChild(e, "blogTitle");
-          var image = findChild(e, "blogImage");
+
           if (bd) {
                     title.textContent = bd.result.title;
                     for (var _index = 0 ; _index < bd.result.content.length; _index++) {
+                              if (bd.result.content[_index].tag == "p" && bd.result.content[_index].children[0].tag != "br") {
                               if (bd.result.content[_index].tag == "p") {
                                         var _text = text.cloneNode(true);
                                         text.parentNode.appendChild(_text);
@@ -31,11 +28,10 @@ window.onload = function() {
                               if (bd.result.content[_index].tag == "figure") {
                                         var _image = image.cloneNode(true);
                                         image.parentNode.appendChild(_image);
+                                        _image.src = "https://telegra.ph" + bd.result.content[_index].children[0].attrs.src;
                                         _image.src = bd.result.content[_index].children[0].attrs.src;
                               }
                     }
           }
-          text.remove();
-          image.remove();
-          
+}
 }
